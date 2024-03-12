d6f466d7f4ef2e5a36dbef9f88f8e84ef33657d7
Slepov Artem <48179773+SlepovEltech@users.noreply.github.com>
2024-03-12 11:05:10 +0300

    Update general.h

--- general.h
+++ general.h
@@ -1,69 +1,20 @@
-// #define PWR_BTN_PORT GPIOA
-// #define PWR_BTN_PIN GPIO_Pin_2
-
-// #define LED_GOOD_PORT GPIOA
-// #define LED_GOOD_PIN GPIO_Pin_3
-
-// #define WDOG_IN_PORT GPIOB
-// #define WDOG_IN_PIN GPIO_Pin_0
-
-// #define WDOG_OUT_PORT GPIOC
-// #define WDOG_OUT_PIN GPIO_Pin_4
-
-// #define ENABLE_DCDC_PORT GPIOB 
-// #define ENABLE_DCDC_PIN GPIO_Pin_1
-
-// #define ENABLE_5V_PORT GPIOB 
-// #define ENABLE_5V_PIN GPIO_Pin_2
-
-// #define PG_SMARC_PORT GPIOB 
-// #define PG_SMARC_PIN GPIO_Pin_3
-
-// #define PG_5V_PORT GPIOB
-// #define PG_5V_PIN GPIO_Pin_4
-
-// #define CARRIER_PWR_ON_PORT GPIOB
-// #define CARRIER_PWR_ON_PIN GPIO_Pin_5
-
-// #define POWER_CPU_PORT GPIOB
-// #define POWER_CPU_PIN GPIO_Pin_6
-
-// #define RESET_CPU_PORT GPIOB
-// #define RESET_CPU_PIN GPIO_Pin_7
-
-// #define LED_PWR_PORT GPIOD
-// #define LED_PWR_PIN 0
-
 
 #define PWR_BTN GPIOA, GPIO_Pin_2
-
 #define LED_GOOD GPIOA, GPIO_Pin_3
-
 #define WDOG_IN GPIOB, GPIO_Pin_0
- 
 #define WDOG_OUT GPIOC, GPIO_Pin_4
-
 #define ENABLE_DCDC GPIOB, GPIO_Pin_1 
-
 #define ENABLE_5V GPIOB, GPIO_Pin_2  
-
 #define PG_SMARC GPIOB, GPIO_Pin_3 
-
 #define PG_5V GPIOB, GPIO_Pin_4
-
 #define CARRIER_PWR_ON GPIOB, GPIO_Pin_5
-
 #define POWER_CPU GPIOB, GPIO_Pin_6
-
 #define RESET_CPU GPIOB, GPIO_Pin_7
-
 #define LED_PWR GPIOD, GPIO_Pin_0
 
 #define B_PWR_MASK			(0)
 #define B_PG_5V_MASK		(1)
 #define B_PG_SMARC_MASK		(2)
-
-
 
 #define TIMER_60_SEC (6000)
 #define TIMER_30_SEC (3000)
